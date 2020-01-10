2f7c48cd719a7b52dc8648fca5f317b29eb40021
Sempredic <vincent.espinoza@yahoo.com>
2020-01-10 03:44:05 -0600

    1st
    asdf

---
+++ AppSuiteWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppSuiteWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
